0a9f2fd9a5026b557aca989a7f6108c239d9755f
Federom99 <federo.15.99@gmail.com>
2022-06-03 21:46:38 -0300

    rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const { Router } = require('express');
+const axios = require ("axios")
+const {KEY_API } = process.env;
+const {Videogame, Genero} = require("../db.js")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +12,49 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+
+const allinfo = async () =>{
+    try {
+        var total = []
+        for(let i=1; i <=5; i++){
+            const urlApi = await axios.get(`https://api.rawg.io/api/games?key=${KEY_API}&page=${i}`);
+            let infoApi = urlApi.data.results.map((e)=>{
+                return{
+                    id: e.id,
+                    name: e.name,
+                    description: e.description,
+                    release_date: e.release_date,
+                    raiting: e.raiting,
+                    plataform: e.plataform,
+                    genres: e.genres?.map((e)=>e.name),
+
+                }
+            })
+            total= total.concat(infoApi)
+        }
+        return total;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const getInfoDB = async () =>{
+    return await Videogame.findAll({
+        incluide:{
+            model: Genero,
+            attributes: ['name'],
+            through:{
+                attributes: [],
+            },
+        },
+    });
+}
+
+const mergeInfo = async()=>{
+    const infoApi = await this.allinfo();
+    const infoDB = await getInfoDB();
+    const infoMerge = infoApi.concat(infoDB);
+    return infoMerge;
+}
 
 module.exports = router;
